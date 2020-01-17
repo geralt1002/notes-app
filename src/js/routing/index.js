ec86d35a3fc2df4ccc7470536bc8d76a62d04969
Krzysiek <krzysztof.uminski1@gmail.com>
2020-01-17 22:07:35 +0100

    fixed routing error

--- src/js/routing/index.js
+++ src/js/routing/index.js
@@ -2,6 +2,7 @@ import page from 'page';
 import * as routes from './routes';
 
 page('/', routes.appView);
+page('/appView', routes.appView);
 page('/appView/:id', routes.appView);
 
 page('*', routes.notFound);
