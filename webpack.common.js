2697faf368aeb5c0fd27ff7153d7e86e54818020
Krzysiek <krzysztof.uminski1@gmail.com>
2021-08-08 17:08:21 +0200

    Upgraded aplication to webpack 5

---
+++ webpack.common.js
@@ -0,0 +1,147 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+const dirNode = 'node_modules';
+const dirApp = path.join(__dirname, 'src');
+const dirStyles = path.join(__dirname, 'scss');
+// const dirAssets = path.join(__dirname, 'assets');
+
+/**
+ * Webpack Configuration
+ */
+module.exports = env => {
+  // Is the current build a development build
+  const IS_DEV = !!env.dev;
+
+  return {
+
+    entry: {
+      main: path.join(dirApp, 'index')
+    },
+
+    resolve: {
+      modules: [
+        dirNode,
+        dirApp,
+        dirStyles
+        //  dirAssets
+      ]
+    },
+
+    plugins: [
+      new webpack.DefinePlugin({ IS_DEV }),
+
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, './src/index.html')
+      //   title: 'Webpack Boilerplate'
+      }),
+
+      new ImageMinimizerPlugin({
+        minimizerOptions: {
+          plugins: [
+            [
+              'imagemin-svgo',
+              {
+                plugins: [
+                  // SVGO options: "https://github.com/svg/svgo#what-it-can-do"
+                  {
+                    removeViewBox: false,
+                    removeXMLNS: true
+                  }
+                ]
+              }
+            ]
+          ]
+        }
+      })
+    ],
+
+    module: {
+      rules: [
+        // BABEL
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              compact: true
+            }
+          }
+        },
+
+        // STYLES
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: IS_DEV
+              }
+            }
+          ]
+        },
+
+        // CSS / SASS
+        {
+          test: /\.scss/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: IS_DEV
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: IS_DEV
+                //  sassOptions: {
+                //    includePaths: [dirAssets]
+                //  }
+              }
+            }
+          ]
+        },
+
+        // IMAGES
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]'
+            }
+          }
+        },
+
+        // SVG
+        {
+          test: /\.svg$/,
+          use: [
+            'raw-loader'
+          ]
+        },
+        {
+          test: /\.(html)$/,
+          use: {
+            loader: 'html-loader',
+            options: {
+              //  minimize: true
+            }
+          }
+        }
+      ]
+    },
+
+    optimization: {
+      runtimeChunk: 'single'
+    }
+
+  };
+};
